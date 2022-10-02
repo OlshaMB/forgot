174daee4486f73be317b79d15855019c1cd3cd02
OlshaMB <michaelolshanskiy@icloud.com>
2022-10-02 13:51:47 +0300

    reformat!: cargo fmt

--- src/args.rs
+++ src/args.rs
@@ -1,15 +1,12 @@
-use clap::{command, Parser, Subcommand};
+use crate::subcommands::list;
 use clap::Command;
+use clap::{command, Parser, Subcommand};
 use enum_dispatch::enum_dispatch;
-use crate::subcommands::{
-    //ui,
-    list
-};
 #[derive(Parser)]
 #[command(author = "OlshaMB", version = "0.5", about = "A todo list app that indexes your app to find TODO:'s", long_about = None, propagate_version = true)]
 pub struct Arguments {
     #[clap(subcommand)]
-    command: Commands
+    command: Commands,
 }
 #[enum_dispatch(Commands)]
 pub trait SubCommandWithFunction {
@@ -20,8 +17,8 @@ pub trait SubCommandWithFunction {
 #[enum_dispatch]
 enum Commands {
     //UI(ui::UI),
-    List(list::List)
+    List(list::List),
 }
-pub fn use_command(args:Arguments){
+pub fn use_command(args: Arguments) {
     args.command.on_use(&args)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ mod subcommands;
 mod todos_search;
 
 fn main() {
-
-
     let args = Arguments::parse();
     use_command(args)
 }
