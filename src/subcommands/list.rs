f85dea24a0af9083bbcf1f3ab600331c0cfd79ab
OlshaMB <michaelolshanskiy@icloud.com>
2022-10-04 11:59:56 +0300

    feat: Adds few options and refactors todo_search
    - Adds use_cl(Use content_line instead of content), display_cl(display content line after poistion) option
    - Todo Item now has content_line properety which conatains whole line where todo is located
    - Refactored code in todo_search to be more simple and more divded and also to not contatain visit_dirs which made also an ability to remove rust licence
    - Changed regex to find NotImplemented todos
    - Bumped version to v0.2.0
    
    Author: OlshaMB
    Version: 0.2.0

--- src/subcommands/list.rs
+++ src/subcommands/list.rs
@@ -8,26 +8,33 @@ use std::path::{Path, PathBuf};
 #[derive(Args)]
 #[command(about = "List all your todos")]
 pub struct List {
-    #[arg(short, long, help = "files/dirs to ignore separated by comma(,)")]
-    ignore: Option<String>,
+    #[arg(short, long, help = "files/dirs to ignore")]
+    ignore: Vec<PathBuf>,
+    #[arg(help="A path where to search")]
+    path: Option<PathBuf>,
+    #[arg(short='c', long="use_cl", help="Replaces content of todo item with whole line where todo is located", default_value_t=false)]
+    use_content_line_as_content:bool,
+    #[arg(short='l', long="cl", help="Shows whole line where todo is located after todo item", default_value_t=false)]
+    display_content_line: bool
 }
 impl SubCommandWithFunction for List {
     fn on_use(&self, args: &Arguments) {
         for todo in todos_search(
-            self.ignore
-                .clone()
-                .unwrap_or("".to_string())
-                .split(",")
-                .map(|s| Path::new(s).to_path_buf())
-                .collect::<Vec<PathBuf>>()
-                .as_ref(),
+            if self.ignore.is_empty() { vec![] } else { self.ignore.clone() },
+            self.path.clone().unwrap_or(PathBuf::from("."))
         ) {
+
             println!(
-                " {} - {}:{}:{}",
-                todo.content.grey(),
-                todo.path.red(),
-                todo.line.to_string().blue(),
-                todo.col.to_string().yellow()
+                " {} - {}:{}:{}{}",
+                if !self.use_content_line_as_content {&todo.content} else {&todo.line_content},
+                &todo.path.dark_cyan(),
+                &todo.line.to_string().dark_blue(),
+                &todo.col.to_string().dark_yellow(),
+                (if self.display_content_line
+                {"\n".to_string() + todo.line_content.clone().trim_start()}
+                else
+                {"".to_string()})
+                                    .dark_grey()
             )
         }
     }
--- src/todos_search.rs
+++ src/todos_search.rs
@@ -1,66 +1,63 @@
-use crossterm::style::Stylize;
-use regex::Regex;
-use std::fmt::format;
-use std::fs::DirEntry;
-use std::ops::Index;
+use regex::{Match, Regex};
 use std::path::{Path, PathBuf};
-use std::{fs, io};
+use std::{fs::{read_dir, read_to_string}, io};
 #[derive(Default)]
 pub struct Todo {
     pub path: String,
     pub line: usize,
     pub col: usize,
     pub content: String,
+    pub line_content: String
 }
-//TODO: remove todos argument
-fn visit_dirs(
-    dir: &Path,
-    cb: &dyn Fn(&DirEntry, &mut Vec<Todo>),
-    todos: &mut Vec<Todo>,
-    ignore: &Vec<PathBuf>,
-) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            //println!("{} - {}", path.to_str().unwrap(), ignore.contains(&path).to_string().bold().blue());
-            if ignore.contains(&path) {
-                continue;
-            }
-            if path.is_dir() {
-                visit_dirs(&path, cb, todos, &ignore)?;
+pub fn find_todos_in_files(path: &Path,todos: &mut Vec<Todo>){
+    let text: String = read_to_string(path).unwrap_or("".to_string());
+    let lines = text.lines().enumerate();
+    let search_regex = Regex::new(r"todo!?\(\x{22}?(.*)?\x{22}?\)|(?i)TODO(?-i):? *(.*)|(?i)NotImplemented(?:Erro?r?)?(?-i)\(?\x{22}?(.*)?\x{22}?\)").unwrap();
+    for (i, line) in lines {
+        for capture in search_regex.captures_iter(line) {
+            let mut todo = Todo::default();
+            todo.line = i + 1;
+            todo.col = capture.get(0).unwrap().start();
+            todo.path = path.to_str().unwrap().to_string();
+            todo.line_content = line.to_string();
+            //println!("[DEBUG]{}", if capture.get(2).is_some() {capture.get(2).unwrap().as_str().to_string()} else {capture.len().to_string()});
+            let content = capture.iter().enumerate().filter(|el|{
+                if el.0==0 { return false; };
+                return el.1.is_some();
+            }).map(|el| el.1).nth(0);
+            if content.is_some() {
+
+                todo.content = if content.unwrap().is_some() { content.unwrap().unwrap().as_str().to_string() } else { "todo".to_string() }
             } else {
-                cb(&entry, todos);
+                todo.content = "todo".to_string();
             }
+            todos.push(todo);
         }
     }
-    Ok(())
 }
-pub(crate) fn todos_search(ignore: &Vec<PathBuf>) -> Vec<Todo> {
-    let mut todos: Vec<Todo> = vec![];
-    let search_regex = Regex::new(r"TODO:? *(.*)|todo!?\(?\x{22}?(.*)?\x{22}?\)").unwrap();
-    visit_dirs(
-        Path::new("."),
-        &|entr, todos| {
-            let text: String = fs::read_to_string(entr.path()).unwrap_or("".to_string());
-            for (i, line) in text.lines().enumerate() {
-                for capture in search_regex.captures_iter(line) {
-                    let mut todo = Todo::default();
-                    todo.line = i + 1;
-                    todo.col = capture.get(0).unwrap().start();
-                    todo.path = entr.path().to_str().unwrap().to_string();
-                    if capture.get(1).is_some() {
-                        todo.content = capture.get(1).unwrap().as_str().to_string()
-                    } else {
-                        todo.content = "todo".to_string();
-                    }
-                    todos.push(todo);
+pub fn index_dirs(path: &Path, ignore: &Vec<PathBuf>, todos: &mut Vec<Todo>){
+    if ignore.contains(&path.to_path_buf()) {
+        return;
+    }
+    if path.is_dir(){
+        let posibly_dir_entries = path.read_dir();
+        if posibly_dir_entries.is_ok() {
+            for entry in posibly_dir_entries.unwrap() {
+                if entry.is_ok() {
+                    index_dirs(&entry.unwrap().path().to_path_buf(), &ignore, todos)
                 }
             }
-        },
-        &mut todos,
+        }
+    } else {
+        find_todos_in_files(path, todos)
+    }
+}
+pub(crate) fn todos_search(ignore: Vec<PathBuf>, path: PathBuf) -> Vec<Todo> {
+    let mut todos: Vec<Todo> = vec![];
+    index_dirs(
+        path.as_path(),
         &ignore,
-    )
-    .expect("");
+        &mut todos
+    );
     return todos;
 }
